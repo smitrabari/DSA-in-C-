fadd9aa0657e98d805c032ad2545de39328a0c52
smitrabari <smitrabari18@gmail.com>
2026-06-24 13:58:51 +0530

    Added Patterns

---
+++ Patterns/p22.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void smit(int n)
+{
+	for(int i = 0; i < 2*n-1 ; i++)
+	{
+		for(int j = 0 ; j < 2*n-1 ; j++)
+		{
+			int top = i;
+			int left = j;
+			int right = (2*n-2)-j;
+			int bottom = (2*n-2)-i;
+			cout<<(n-min(min(top,bottom),min(left,right)));
+		}
+		cout<<"\n";
+	}
+	
+}
+
+int main()
+{
+	int n;
+	cout<<"Enter Number - ";
+	cin>>n;
+	smit(n);
+	return 0;
+}
