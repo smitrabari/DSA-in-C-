e491d2d975112941b8528b972d852aefed01c11b
smitrabari <smitrabari18@gmail.com>
2026-07-01 22:48:03 +0530

    Digit Extraction

---
+++ Basic Math Problems/Digit_Extraction.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Digit_Extraction(int n)
+{
+	while(n>0)
+	{
+		for(int i = 0;n>0;i++)
+		{
+			int lastdigit = n % 10;
+			cout<<lastdigit<<endl;
+			n = n / 10;
+		}
+	}
+
+}
+
+int main()
+{
+	int n;
+	cout<<"Enter the Number - ";
+	cin>>n;
+	Digit_Extraction(n);
+	return 0;
+}
