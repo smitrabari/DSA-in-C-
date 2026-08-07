7d20ed50de94b9c7ab7877aebeb96a441f9a01e2
smitrabari <smitrabari18@gmail.com>
2026-08-07 20:21:01 +0530

    Insertion Sort

--- Sorting/Sorting - I/Insertion_Sort.cpp
+++ Sorting/Sorting - I/Insertion_Sort.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 void insertion_sort(int arr[],int n) //function declaration!
 {
-	
+	for(int i = 0;i <= n - 1;i++)
+	{
+		int j = i;
+		while(j>0 && arr[j-1]>arr[j]) //condition!!
+		{
+			swap(arr[j-1],arr[j]); //swap!!
+			j--;
+		}
+	}
 }
 
 int main()
