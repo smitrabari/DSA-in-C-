e311319440a54acdcf6c44dd0851a55cf0c25dd9
smitrabari <smitrabari18@gmail.com>
2026-08-14 09:51:23 +0530

    Array is Sorted or not

--- Array/Easy/Array_Sorted_or_not.cpp
+++ Array/Easy/Array_Sorted_or_not.cpp
@@ -6,9 +6,9 @@ int main()
 	int n = 9; //size of an array
 	int arr[n] = {20,34,12,53,65,1,2,8,73}; //elements of an array
 	string result;
-	for(int i = 1; i < n ; i++)
+	for(int i = 1; i < n ; i++) //for each loop(start from 1)
 	{
-		if(arr[i]>=arr[i-1])
+		if(arr[i]>=arr[i-1]) // condition of comparing two elements on row
 		{
 
 		}
