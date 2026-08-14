a8b6ef121cf9b484d7ab9bc940edf359aa667c5d
smitrabari <smitrabari18@gmail.com>
2026-08-14 09:31:23 +0530

    Largest Element

--- Array/Easy/Largest_number.cpp
+++ Array/Easy/Largest_number.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-	int n = 9;
-	int arr[n] = {20,34,12,53,65,1,2,8,73};
-	int largest = arr[0];
-	for(int i = 0;i<n;i++)
+	int n = 9; //size of array
+	int arr[n] = {20,34,12,53,65,1,2,8,73}; //elements of array
+	int largest = arr[0]; //largest element take it as first element as refrence
+	for(int i = 0;i<n;i++) //for each loop
 	{
-		if(arr[i]>largest)
+		if(arr[i]>largest) //condition
 		{
-			largest = arr[i];
+			largest = arr[i]; //value asigning
 		}
 	}
 
