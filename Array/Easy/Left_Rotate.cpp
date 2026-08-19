491c2f1ad5b695dc6aa7d1dfeeb40fd3e41e9170
smitrabari <smitrabari18@gmail.com>
2026-08-19 23:52:30 +0530

    Left Rotate

--- Array/Easy/Left_Rotate.cpp
+++ Array/Easy/Left_Rotate.cpp
@@ -6,12 +6,12 @@ int main()
 	int n = 5; //size of array
 	int arr[n] = {1,2,3,4,5}; //elements of array
 	int temp = arr[0]; // creaeing a temp. variable for storeing the last value
-	for(int i = 1;i < n;i++)
+	for(int i = 1;i < n;i++) // for loop for rotateing
 	{
 		arr[i-1]=arr[i];
 	}
-	arr[n-1]=temp;
-	for(int i = 0; i< n;i++)
+	arr[n-1]=temp; //asign the last value
+	for(int i = 0; i< n;i++) //for each loop
 	{
 		cout<<arr[i]<<" ";
 	}
